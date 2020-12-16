8bb84cb36f854594036c97c278cc02ab89a1a474
LukasZ <llo@bluewin.ch>
2020-12-16 19:38:50 +0100

    In motor_controller.py: removed pulldowns in GPIO inputs. Added a few pint statement (commented out) for debugging. Added line breaks to comments to limit the width of texts.

--- phenobox/phenobox/gpio_controllers/motor_controller.py
+++ phenobox/phenobox/gpio_controllers/motor_controller.py
@@ -9,32 +9,50 @@ class MotorController:
     All lines, except the ALARM line are active high.
     """
     # Pin declarations
-    #: The GPIO Pin to which the SVON line of the motor controller is connected. Used to turn the servo on. Active High.
+    #: The GPIO Pin to which the SVON line of the motor controller is connected.
+    # Used to turn the servo on. Active High.
     _SVON = 17
-    #: The GPIO Pin to which the IN0 line of the motor controller is connected. Used to send position to the controller
+
+    #: The GPIO Pin to which the IN0 line of the motor controller is connected.
+    # Used to send position to the controller
     _IN0 = 22
-    #: The GPIO Pin to which the IN1 line of the motor controller is connected. Used to send position to the controller
+
+    #: The GPIO Pin to which the IN1 line of the motor controller is connected.
+    # Used to send position to the controller
     _IN1 = 23
-    #: The GPIO Pin to which the IN2 line of the motor controller is connected. Used to send position to the controller
+
+    #: The GPIO Pin to which the IN2 line of the motor controller is connected.
+    # Used to send position to the controller
     _IN2 = 24
-    #: The GPIO Pin to which the ENABLE line of the motor controller is connected. Used to enable to controller.
+
+    #: The GPIO Pin to which the ENABLE line of the motor controller is connected.
+    # Used to enable to controller.
     _ENABLE = 26
-    #: The GPIO Pin to which the SETUP line of the motor controller is connected. Used to initialize the motor controller on startup
+
+    #: The GPIO Pin to which the SETUP line of the motor controller is connected.
+    # Used to initialize the motor controller on startup
     _SETUP = 27
-    #: The GPIO Pin to which the DRIVE line of the motor controller is connected. Used to start a movement. Active High
+    #: The GPIO Pin to which the DRIVE line of the motor controller is connected.
+    # Used to start a movement. Active High
     _DRIVE = 18
-    #: The GPIO Pin to which the RESET line of the motor controller is connected. Used to reset the Alarm state.
+    #: The GPIO Pin to which the RESET line of the motor controller is connected.
+    # Used to reset the Alarm state.
     _RESET = 25
 
-    #: The GPIO Pin to which the ALARM line of the motor controller is connected. If this gets low the motor emits an ALARM condition
+    #: The GPIO Pin to which the ALARM line of the motor controller is connected.
+    # If this gets low the motor emits an ALARM condition
     _ALARM = 21
-    #: The GPIO Pin to which the INP line of the motor controller is connected. Used by the controller to signal that it has reached the target position
+    #: The GPIO Pin to which the INP line of the motor controller is connected.
+    # Used by the controller to signal that it has reached the target position
     _INP = 20
-    #: The GPIO Pin to which the SVRE line of the motor controller is connected. Used by the controller to indicate that the servo is ready
+    #: The GPIO Pin to which the SVRE line of the motor controller is connected.
+    # Used by the controller to indicate that the servo is ready
     _SVRE = 16
 
-    # TODO listen for ALARM signal of motor controller and take actions accordingly (transition to error)
-    # Pass in a callback to the initialize function which should get called when the ALARM is triggered (Gets low)
+    # TODO listen for ALARM signal of motor controller and take actions accordingly
+    # (transition to error)
+    # Pass in a callback to the initialize function which should get called when
+    # the ALARM is triggered (Gets low)
 
     def __init__(self):
         self._current_angle = -1
@@ -70,22 +88,38 @@ class MotorController:
         GPIO.setup(self._DRIVE, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(self._RESET, GPIO.OUT, initial=GPIO.LOW)
 
-        GPIO.setup(self._ALARM, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(self._INP, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(self._SVRE, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        #GPIO.setup(self._ALARM, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        #GPIO.setup(self._INP, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        #GPIO.setup(self._SVRE, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        # No pullups needed here as the input potentials are set via external resistors.
+        # The new Driver Board has higher resistance devider so the additional pulldown
+        # might reduce input voltage.
+        GPIO.setup(self._ALARM, GPIO.IN)
+        GPIO.setup(self._INP, GPIO.IN)
+        GPIO.setup(self._SVRE, GPIO.IN)
         time.sleep(2)
         if not GPIO.input(self._ALARM):  # ALARM is negative logic
-            print "RESET ALARM"
+            print "Resetting ALARM"
             GPIO.output(self._RESET, GPIO.HIGH)
             self._wait_until(self._ALARM, GPIO.HIGH)
             GPIO.output(self._RESET, GPIO.LOW)
-            print "ALARM RESET"
+            print "ALARM cleared"
+        #else:
+        #    print('ALARM is clear')
 
+        #print('Enable drive')
         GPIO.output(self._ENABLE, GPIO.HIGH)
+        #print('Switching Servo on (SVON)')
         GPIO.output(self._SVON, GPIO.HIGH)
+        #print('waiting for SVRE')
         self._wait_until(self._SVRE, GPIO.HIGH)
+        #print('Searching origin')
         GPIO.output(self._SETUP, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(self._SETUP, GPIO.LOW)
+        #print('waiting for INP')
         self._wait_until(self._INP, GPIO.HIGH)
+        #print('Position reached (INP)')
         GPIO.output(self._SETUP, GPIO.LOW)
         GPIO.output(self._SVON, GPIO.LOW)
         self._current_angle = 0
@@ -110,8 +144,8 @@ class MotorController:
 
     def move_to_position(self, position):
         """
-        Takes in a number indicating the position index and moves the motor to the corresponding position which is programmed
-        into the motor controller.
+        Takes in a number indicating the position index and moves the motor to the
+        corresponding position which is programmed into the motor controller.
         This method blocks until the motor has reached the given position.
 
         :param position: The index of the position to move the motor to

f74aade08d065e42a7a0c7ddad73b771d8d14717
LukasZ <llo@bluewin.ch>
2020-12-17 17:15:19 +0100

    Fixed camera/errors.py: if CameraError exception has no "detail" attribute the stringyfied exception produced a runtime error.

--- phenobox/phenobox/camera/errors.py
+++ phenobox/phenobox/camera/errors.py
@@ -1,23 +1,21 @@
 class CameraError(Exception):
     def __init__(self, message, detail=None):
         self.message = message
-        if detail:
+        if hasattr(self, 'detail'):
             self.detail = detail
 
     def __str__(self):
-        return '%s - %s' % (self.message, self.detail)
+        if hasattr(self, 'detail'):
+          return '%s - %s' % (self.message, self.detail)
+        else:
+          return '%s' % self.message
 
 
 class CaptureError(CameraError):
     def __init__(self, message, detail=None):
         super(CaptureError, self).__init__(message, detail)
 
-    def __str__(self):
-        return '%s - %s' % (self.message, self.detail)
-
 class ConnectionError(CameraError):
     def __init__(self, message, detail=None):
         super(ConnectionError, self).__init__(message, detail)
 
-    def __str__(self):
-        return '%s - %s' % (self.message, self.detail)

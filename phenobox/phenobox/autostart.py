d27dc09616bf3cb14ea2752950b3e948d48f927e
LukasZ <llo@bluewin.ch>
2020-12-30 14:58:06 +0100

    autostart.py was not working: There is no module photobox, phenobox seems to be meant; The Phenobox class needs to be instantiated. Changed the configuration file name to 'production_config.ini'.

--- phenobox/phenobox/autostart.py
+++ phenobox/phenobox/autostart.py
@@ -4,11 +4,12 @@ import time
 
 import sys
 
-import photobox
+from phenobox import Phenobox
 from config import config
 
 config.load_config('{}/{}'.format('config', 'production_config.ini'))
 while not os.path.ismount(getattr(config, 'cfg').get('box', 'shared_folder_mountpoint')):
     time.sleep(1)
 
-photobox.run()
+phenobox = Phenobox()
+phenobox.run()
